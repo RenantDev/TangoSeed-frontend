9f83de2a3cb3ad484e94db5224a63d9c63f0aa88
Renant Bernabe <contato@renant.com.br>
2019-11-16 10:58:46 -0500

    ajustes

--- src/app/views/admin/groups/group-table/group-table.component.ts
+++ src/app/views/admin/groups/group-table/group-table.component.ts
@@ -7,60 +7,225 @@ declare var $: any;
   selector: 'app-group-table',
   templateUrl: './group-table.component.html',
   styleUrls: ['./group-table.component.css'],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
 })
 export class GroupTableComponent implements OnInit {
 
-  dataSource = ELEMENT_DATA;
-  columnsToDisplay = ['id', 'name', 'count_users', 'status', 'menu'];
-  expandedElement: PeriodicElement | null;
+  dataSource = GROUPS_DATA;
+  groupList: GroupList | null;
+
 
   constructor() {
   }
 
   ngOnInit() {
-
+    $('.dual_select').bootstrapDualListbox({
+      selectorMinimalHeight: 160
+    });
   }
 
+
 }
 
-export interface PeriodicElement {
+export interface GroupList {
   id: number;
   name: string;
-  count_users: number;
   status: number;
   description: string;
+  roles: any;
+  roles_select: any;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
+const GROUPS_DATA: GroupList[] = [
   {
     id: 1,
     name: 'Developer',
-    count_users: 1,
     status: 1,
     description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`,
+    roles: [
+      {
+        id: 1,
+        title: 'Resumo Dev',
+        description: 'Função para Desenvolvedor',
+        group_id: 1,
+        role_id: 2,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      },
+      {
+        id: 2,
+        title: 'Administrador',
+        description: 'Administração do sistema.',
+        group_id: 2,
+        role_id: 3,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      },
+      {
+        id: 3,
+        title: 'Usuários',
+        description: 'Gerenciador de usuários do sistema',
+        group_id: 2,
+        role_id: 4,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      },
+      {
+        id: 4,
+        title: 'Grupos',
+        description: 'Gerenciador de grupos do sistema',
+        group_id: 2,
+        role_id: 5,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      },
+      {
+        id: 5,
+        title: 'Funções',
+        description: 'Gerenciador de funções do sistema',
+        group_id: 2,
+        role_id: 6,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      },
+      {
+        id: 6,
+        title: 'Extenções',
+        description: 'Gerenciador de extenções do sistema',
+        group_id: 2,
+        role_id: 7,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      }
+    ],
+    roles_select: []
   }, {
     id: 2,
     name: 'Administrator',
-    count_users: 15,
     status: 1,
     description: `Helium is a chemical element with symbol He and atomic number 2. It is a
         colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
-        group in the periodic table. Its boiling point is the lowest among all the elements.`
+        group in the periodic table. Its boiling point is the lowest among all the elements.`,
+    roles: [
+      {
+        id: 1,
+        title: 'Resumo Dev',
+        description: 'Função para Desenvolvedor',
+        group_id: 1,
+        role_id: 2,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      },
+      {
+        id: 2,
+        title: 'Administrador',
+        description: 'Administração do sistema.',
+        group_id: 2,
+        role_id: 3,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      },
+      {
+        id: 3,
+        title: 'Usuários',
+        description: 'Gerenciador de usuários do sistema',
+        group_id: 2,
+        role_id: 4,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      },
+      {
+        id: 4,
+        title: 'Grupos',
+        description: 'Gerenciador de grupos do sistema',
+        group_id: 2,
+        role_id: 5,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      },
+      {
+        id: 5,
+        title: 'Funções',
+        description: 'Gerenciador de funções do sistema',
+        group_id: 2,
+        role_id: 6,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      },
+      {
+        id: 6,
+        title: 'Extenções',
+        description: 'Gerenciador de extenções do sistema',
+        group_id: 2,
+        role_id: 7,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      }
+    ],
+    roles_select: []
   }, {
     id: 3,
     name: 'Client',
-    count_users: 55000,
     status: 1,
     description: `Lithium is a chemical element with symbol Li and atomic number 3. It is a soft,
         silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
-        lightest solid element.`
+        lightest solid element.`,
+    roles: [
+      {
+        id: 1,
+        title: 'Resumo Dev',
+        description: 'Função para Desenvolvedor',
+        group_id: 1,
+        role_id: 2,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      },
+      {
+        id: 2,
+        title: 'Administrador',
+        description: 'Administração do sistema.',
+        group_id: 2,
+        role_id: 3,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      },
+      {
+        id: 3,
+        title: 'Usuários',
+        description: 'Gerenciador de usuários do sistema',
+        group_id: 2,
+        role_id: 4,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      },
+      {
+        id: 4,
+        title: 'Grupos',
+        description: 'Gerenciador de grupos do sistema',
+        group_id: 2,
+        role_id: 5,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      },
+      {
+        id: 5,
+        title: 'Funções',
+        description: 'Gerenciador de funções do sistema',
+        group_id: 2,
+        role_id: 6,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      },
+      {
+        id: 6,
+        title: 'Extenções',
+        description: 'Gerenciador de extenções do sistema',
+        group_id: 2,
+        role_id: 7,
+        created_at: '2019-05-29 16:26:04',
+        updated_at: '2019-05-29 16:26:04'
+      }
+    ],
+    roles_select: []
   },
 ];
